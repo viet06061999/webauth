5ccd03d20209c8024aad8376133d354eaa99144a
Glitch (webauthn-codelab-start) <none>
2020-09-30 05:57:16 +0000

    ⏳🏅 Checkpoint ./webpack.config.js:343052/262 ./server.js:343052/20025 ./package.json:343052/1561 ./libs/auth.js:343052/26738 ./public/client.js:343052/1 ./public/components.js:343052/134 ./public/style.css:343052/2315

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./public/components.js",
+  mode: "production",
+  output: {
+    filename: "components-bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /components\.js$/,
+      loader: 'babel-loader',
+      query: {presets: ['env']}
+    }]
+  }
+};
