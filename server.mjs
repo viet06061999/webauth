5095779629b3247b4c4143e22a94659b87b70874
Glitch (webauthn-codelab-start) <none>
2024-01-17 10:52:33 +0000

    🌦🚦 Checkpoint ./server.mjs:64298671/7289

--- server.mjs
+++ server.mjs
@@ -16,29 +16,31 @@
  */
 
 // init project
-import express from 'express';
-import session from 'express-session';
-import hbs from 'hbs';
-import { auth } from './libs/auth.mjs';
+import express from "express";
+import session from "express-session";
+import hbs from "hbs";
+import { auth } from "./libs/auth.mjs";
 const app = express();
 
-app.set('view engine', 'html');
-app.engine('html', hbs.__express);
-app.set('views', './views');
+app.set("view engine", "html");
+app.engine("html", hbs.__express);
+app.set("views", "./views");
 app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static('dist'));
-app.use(session({
-  secret: 'secret', // You should specify a real secret here
-  resave: true,
-  saveUninitialized: false,
-  proxy: true,
-  cookie:{
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none'
-  }
-}));
+app.use(express.static("public"));
+app.use(express.static("dist"));
+app.use(
+  session({
+    secret: "secret", // You should specify a real secret here
+    resave: true,
+    saveUninitialized: false,
+    proxy: true,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    },
+  })
+);
 
 app.use((req, res, next) => {
   if (process.env.PROJECT_DOMAIN) {
@@ -46,62 +48,62 @@ app.use((req, res, next) => {
   } else {
     process.env.HOSTNAME = req.headers.host;
   }
-  const protocol = /^localhost/.test(process.env.HOSTNAME) ? 'http' : 'https';
+  const protocol = /^localhost/.test(process.env.HOSTNAME) ? "http" : "https";
   process.env.ORIGIN = `${protocol}://${process.env.HOSTNAME}`;
   if (
-    req.get('x-forwarded-proto') &&
-    req.get('x-forwarded-proto').split(',')[0] !== 'https'
+    req.get("x-forwarded-proto") &&
+    req.get("x-forwarded-proto").split(",")[0] !== "https"
   ) {
     return res.redirect(301, process.env.ORIGIN);
   }
-  req.schema = 'https';
+  req.schema = "https";
   next();
 });
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // Check session
   if (req.session.username) {
     // If user is signed in, redirect to `/reauth`.
-    res.redirect(307, '/reauth');
+    res.redirect(307, "/reauth");
     return;
   }
   // If user is not signed in, show `index.html` with id/password form.
-  res.render('index.html');
+  res.render("index.html");
 });
 
-app.get('/home', (req, res) => {
-  if (!req.session.username || req.session['signed-in'] != 'yes') {
+app.get("/home", (req, res) => {
+  if (!req.session.username || req.session["signed-in"] != "yes") {
     // If user is not signed in, redirect to `/`.
-    res.redirect(307, '/');
+    res.redirect(307, "/");
     return;
   }
   // `home.html` shows sign-out link
-  res.render('home.html', { username: req.session.username });
+  res.render("home.html", { username: req.session.username });
 });
 
-app.get('/reauth', (req, res) => {
+app.get("/reauth", (req, res) => {
   const username = req.session.username;
   if (!username) {
-    res.redirect(302, '/');
+    res.redirect(302, "/");
     return;
   }
   // Show `reauth.html`.
   // User is supposed to enter a password (which will be ignored)
   // Make XHR POST to `/signin`
-  res.render('reauth.html', { username: username });
+  res.render("reauth.html", { username: username });
 });
 
-app.get('/.well-known/assetlinks.json', (req, res) => {
+app.get("/.well-known/assetlinks.json", (req, res) => {
   const assetlinks = [];
   const relation = [
-    'delegate_permission/common.handle_all_urls',
-    'delegate_permission/common.get_login_creds',
+    "delegate_permission/common.handle_all_urls",
+    "delegate_permission/common.get_login_creds",
   ];
   assetlinks.push({
     relation: relation,
     target: {
-      namespace: 'web',
+      namespace: "web",
       site: process.env.ORIGIN,
     },
   });
@@ -109,7 +111,7 @@ app.get('/.well-known/assetlinks.json', (req, res) => {
     assetlinks.push({
       relation: relation,
       target: {
-        namespace: 'android_app',
+        namespace: "android_app",
         package_name: process.env.ANDROID_PACKAGENAME,
         sha256_cert_fingerprints: [process.env.ANDROID_SHA256HASH],
       },
@@ -118,10 +120,10 @@ app.get('/.well-known/assetlinks.json', (req, res) => {
   res.json(assetlinks);
 });
 
-app.use('/auth', auth);
+app.use("/auth", auth);
 
 // listen for req :)
 const port = process.env.GLITCH_DEBUGGER ? null : 8080;
 const listener = app.listen(port || process.env.PORT, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
